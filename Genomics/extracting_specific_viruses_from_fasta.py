08b25ed04738b8d7cd85029d3355f58ed0a96c27
Tony Shen <tsa87@sfu.ca>
2020-02-18 14:59:34 -0800

    Prevent repeating items by using Set

--- Genomics/extracting_specific_viruses_from_fasta.py
+++ Genomics/extracting_specific_viruses_from_fasta.py
@@ -5,15 +5,14 @@ import re
 N_SAMPLE = 97
 fasta_file = "coronaviridae.total.genbank2fasta.uniq.0125.fasta"
 
-records = []
+records = set()
 
 for record in SeqIO.parse(fasta_file, 'fasta'):
     mers = re.search("Middle.*Homo_sapiens", record.id)
     if mers is not None:
         records.append(record)
 
-
-sampled_records = sample(records, N_SAMPLE)
+sampled_records = sample(list(records), N_SAMPLE)
 
 with open("reduced_mers.fasta", "w+") as output_handle:
     SeqIO.write(sampled_records, output_handle, "fasta")
