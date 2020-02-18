fcb62d31ecdad8a653e3dafbdc00a4fa2658d8fd
Tony Shen <tsa87@sfu.ca>
2020-02-18 15:09:26 -0800

    Small fix

--- Genomics/extracting_specific_viruses_from_fasta.py
+++ Genomics/extracting_specific_viruses_from_fasta.py
@@ -4,15 +4,19 @@ import re
 
 N_SAMPLE = 97
 fasta_file = "coronaviridae.total.genbank2fasta.uniq.0125.fasta"
+fitler =
 
-records = set()
+records_data = set()
+records_full = []
 
 for record in SeqIO.parse(fasta_file, 'fasta'):
     mers = re.search("Middle.*Homo_sapiens", record.id)
     if mers is not None:
-        records.add(record)
+        if record.seq._data not in records_data:
+            records_data.add(record.seq._data)
+            records_full.append(record)
 
-sampled_records = sample(list(records), N_SAMPLE)
+sampled_records = sample(records_full, N_SAMPLE)
 
 with open("reduced_mers.fasta", "w+") as output_handle:
     SeqIO.write(sampled_records, output_handle, "fasta")
