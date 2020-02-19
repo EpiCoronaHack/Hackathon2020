287b2e7ba8be064a850c2413c5e49494ee0ce125
Lavneet Puri <lavneetpuri@gmail.com>
2020-02-19 03:58:53 -0800

    i1-move helper functions to seperate file for modularization

---
+++ Team Flight/Data/flirt.eha.io/helpers.js
@@ -0,0 +1,19 @@
+// Query the backend for flight information
+function getFlights({airport_code_list, start_date, end_date}) {
+  // This query was built by analyzing the network communication with meteor backend
+  query = {
+    discontinuedDate: {$gte: start_date},
+    effectiveDate: {$lte: end_date},
+    "departureAirport._id": {$in: airport_code_list}
+  }
+
+  // TODO: what is third parameter used for?
+  Meteor.call('flightsByQuery', query, 3000, (err,res)=> {
+    if (err) {
+      console.log("Requests was unsuccessfull.")
+      console.log(err)
+    }
+    console.log(res)
+    flights = res.flights
+  })
+}
