8e2edd6e50be66a845d6905ca77a70a62a1148c2
Ivan <ivan.pmartell@hotmail.com>
2020-02-18 19:14:13 -0800

    Added sars and mers files with updated script for genomes that are close to 30000bp

--- Genomics/extracting_specific_viruses_from_fasta.py
+++ Genomics/extracting_specific_viruses_from_fasta.py
@@ -21,24 +21,27 @@ fasta_file = "/Users/ivan/Downloads/coronaviridae.total.genbank2fasta.uniq.0125.
 
 records_data = set()
 records_full = {}
+lengths = []
 
 for record in SeqIO.parse(fasta_file, 'fasta'):
     #virus = re.search("^Middle.*([12][90][0-9]{2}-[0-9]{2}-[0-9]{2})", record.id)
     virus = re.search("^SARS.*([12][90][0-9]{2}-[0-9]{2}-[0-9]{2})", record.id)
     if virus is not None:
-        if 'like' not in record.seq._data:
-            if do_add(records_data, record.seq._data):
-                dt = time.strptime(virus.group(1), "%Y-%m-%d")
-                dt_int = to_integer(dt)
-                records_full[dt_int] = record
+        if 'like' not in record.id:
+            if len(record.seq._data) > 28000:
+                if do_add(records_data, record.seq._data):
+                    dt = time.strptime(virus.group(1), "%Y-%m-%d")
+                    dt_int = to_integer(dt)
+                    records_full[dt_int] = record
+                else:
+                    print("Duplicate")
             else:
-                print("Duplicate")
+                print("Genome is too small")
         else:
             print("Virus-like but not the same")
 
 sorted_records = collections.OrderedDict(sorted(records_full.items()))
 sorted_records_list = [*sorted_records.values()]
-print(len(sorted_records_list))
 
 #random 120
 #sampled_records = sample(sorted_records_list, N_SAMPLE)
@@ -48,6 +51,13 @@ print(len(sorted_records_list))
 
 #last 120
 sampled_records = sorted_records_list[-N_SAMPLE:]
+
+for r in sampled_records:
+    lengths.append(len(r.seq._data))
+
+print(max(lengths))
+print(min(lengths))
+print(sum(lengths)/len(lengths))
 
 #with open("reduced_mers.fasta", "w+") as output_handle:
 with open("reduced_sars.fasta", "w+") as output_handle:
