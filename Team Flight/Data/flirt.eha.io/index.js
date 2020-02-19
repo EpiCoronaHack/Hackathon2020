71ac258e787a247ebe775d75cedf6520d4cdec4e
Lavneet Puri <lavneetpuri@gmail.com>
2020-02-19 09:04:56 -0800

    i1-update index.js header

--- Team Flight/Data/flirt.eha.io/index.js
+++ Team Flight/Data/flirt.eha.io/index.js
@@ -5,25 +5,28 @@
 /**
  * Script used for fetching flight data from https://flirt.eha.io/
  * 
- *** ------------
+ ***
  * Description:
  * flirt.eha.io uses Meteor for its data-layer. We can exploit its remote 
  * procedure (Meteor methods) and query the backend for flights leaving
  * a given region (city, country, airport, etc).
  * 
- *** ------------
+ ***
  * Usage:
  * 1. Go to https://flirt.eha.io/
  * 2. Open browser console (ctrl-shift-j in chrome)
  * 3. Paste this script and press enter.
  * 4. If successful, the requested flight information will be logged to console.
  * 
- *** ------------
+ ***
  * Airport Codes:
  * For possible list of airport codes, analyze websockets connection while
  * searching for flights through UI by country or city. Look for call to
  * `flightsByQuery` method. You may also find the airport codes from other
  * resources on the internet.
+ * 
+ **********************
+ * Author: Lavneet Puri
  */
 
 /** fetch.js **/
