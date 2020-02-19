ce97c9c09af90c1f7006311c42938faccd452626
Lavneet Puri <lavneetpuri@gmail.com>
2020-02-19 12:28:34 -0800

    i1-linting update

--- Team Flight/Data/flirt.eha.io/helpers.js
+++ Team Flight/Data/flirt.eha.io/helpers.js
@@ -9,25 +9,21 @@ function getSchedules({ airport_codes, start_date, end_date, limit }, callback) 
     discontinuedDate: { $gte: start_date },
     effectiveDate: { $lte: end_date },
     "departureAirport._id": { $in: airport_codes }
-  }
-
+  };
   Meteor.call('flightsByQuery', query, limit, callback);
 }
-
 // Get total number of flight schedules in the database
 function getSchedulesCount({ airport_code_list, start_date, end_date }, callback) {
   query = {
     discontinuedDate: { $gte: start_date },
     effectiveDate: { $lte: end_date },
     "departureAirport._id": { $in: airport_code_list }
-  }
-
+  };
   let count = 0;
   Meteor.call('flightsByQuery', query, count, callback);
 }
-
 /**
- * Transform flights objects recieved from server 
+ * Transform flights objects recieved from server
  * to fit the schema in README.md
  * @param {Array} flights raw flights array from the `flightsByQuery` RPC
  * @param {DateArray} [time_range] filter between given time range [start, end]
@@ -36,28 +32,23 @@ function transform(flights, time_range) {
   schedules = flights.map(fl => {
     // TODO: log number of omitted entries
     // omit flight with missing arrival airport info
-    if (!fl.arrivalAirport) return null;
-
+    if (!fl.arrivalAirport)
+      return null;
     time_range = time_range || [fl.effectiveDate, fl.discontinuedDate];
-
     // get days of week and dates that the flight is scheduled for
     days_scheduled = [];
     for (let i = 1; i <= 7; i++) {
       days_scheduled[i] = fl[`day${i}`];
     }
     dates_scheduled = generateDates(time_range, days_scheduled);
-
     // if flight not running within the specified time_range
-    if (!dates_scheduled.length) return null;
-
+    if (!dates_scheduled.length)
+      return null;
     // concatenate depature time with dates_scheduled to get departure times
     departure_times = [];
     for (i in dates_scheduled) {
-      departure_times.push(
-        new Date(dates_scheduled[i].setHours(...fl.departureTimeUTC.split(':')))
-      );
+      departure_times.push(new Date(dates_scheduled[i].setHours(...fl.departureTimeUTC.split(':'))));
     }
-
     // concatenate arrival time with dates_scheduled to get arrival times
     arrival_times = [];
     for (i in dates_scheduled) {
@@ -68,12 +59,8 @@ function transform(flights, time_range) {
       // day will increment by exactly 1.
       if (fl.arrivalTimeUTC <= fl.departureTimeUTC)
         date = new Date(date.setDate(date.getDate() + 1));
-
-      arrival_times.push(
-        new Date(date.setHours(...fl.arrivalTimeUTC.split(':')))
-      );
+      arrival_times.push(new Date(date.setHours(...fl.arrivalTimeUTC.split(':'))));
     }
-
     // create schedule object
     schedule = {
       flight_id: `${fl.carrier}${fl.flightNumber}`,
@@ -97,18 +84,15 @@ function transform(flights, time_range) {
         times: arrival_times,
         loc: fl.loc,
       },
-    }
-
+    };
     return schedule;
   });
-
   return schedules.filter(schedule => schedule != null);
 }
-
 /**
  * Generate list of dates between given time range.
  * Then filter out dates based on `filter_weekdays` list.
- * 
+ *
  * @param {DateArray} range given time range [start, end]
  * @param {Array} [filter_weekdays] given time range [start, end]
  */
@@ -117,17 +101,16 @@ function generateDates(range, weekdays_to_keep) {
   weekdays_to_keep = weekdays_to_keep || Array(8).fill().map(() => true);
   // Truncate day time from range
   range = range.map(date => new Date(date.toDateString()));
-
-  dates = []
+  dates = [];
   let date = new Date(range[0]);
   let day = date.getDay();
   // iterate through all days in given range and keep only specified weekdays
-  while(date <= range[1]) {
-    if (weekdays_to_keep[day]) dates.push(new Date(date));
+  while (date <= range[1]) {
+    if (weekdays_to_keep[day])
+      dates.push(new Date(date));
     // increment to next day
     date = new Date(date.setDate(date.getDate() + 1));
     day = date.getDay();
   }
-
   return dates;
 }
