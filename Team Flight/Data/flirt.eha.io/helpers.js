bcdb9bbc463a05b7c9fbb77512df40504e1cda30
Lavneet Puri <lavneetpuri@gmail.com>
2020-02-19 05:04:40 -0800

    i1-add transform helper with basic key mapping

--- Team Flight/Data/flirt.eha.io/helpers.js
+++ Team Flight/Data/flirt.eha.io/helpers.js
@@ -11,4 +11,38 @@ function getFlights({ airport_code_list, start_date, end_date }, callback) {
   Meteor.call('flightsByQuery', query, 3000, callback);
 }
 
+/**
+ * Transform flights objects recieved from server 
+ * to fit the schema in README.md
+ * @param {Array} flights 
+ * @param {Date Array} [time_range] filter between given time range [start, end]
+ */
+function transform(flights) {
+  flights.map(fl => {
+    //     
+    flight = {
+      flight_id: `${fl.carrier}${fl.flightNumber}`,
+      total_seats: fl.total_seats,
+      effective_date: fl.effectiveDate,
+      discontinued_date: fl.discontinuedDate,
+      departure: {
+        airport_code: fl.departureAirport._id,
+        city: fl.departureAirport.city,
+        country: fl.departureAirport.countryName,
+        global_region: fl.departureAirport.globalRegion,
+        loc: fl.loc,
+      },
+      arrival: {
+        airport_code: fl.arrivalAirport._id,
+        city: fl.arrivalAirport.city,
+        country: fl.arrivalAirport.countryName,
+        global_region: fl.arrivalAirport.globalRegion,
+        loc: fl.loc,
+      },
+    }
+
+    return flight;
+  });
+
+  return flights;
 }
