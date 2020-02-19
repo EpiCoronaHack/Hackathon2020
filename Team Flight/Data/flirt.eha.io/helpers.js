a212a4f6611e984cb5f442da80f8a51e912af2fc
Lavneet Puri <lavneetpuri@gmail.com>
2020-02-19 04:12:35 -0800

    linting update

--- Team Flight/Data/flirt.eha.io/helpers.js
+++ Team Flight/Data/flirt.eha.io/helpers.js
@@ -1,14 +1,14 @@
 // Query the backend for flight information
-function getFlights({airport_code_list, start_date, end_date}) {
+function getFlights({ airport_code_list, start_date, end_date }) {
   // This query was built by analyzing the network communication with meteor backend
   query = {
-    discontinuedDate: {$gte: start_date},
-    effectiveDate: {$lte: end_date},
-    "departureAirport._id": {$in: airport_code_list}
+    discontinuedDate: { $gte: start_date },
+    effectiveDate: { $lte: end_date },
+    "departureAirport._id": { $in: airport_code_list }
   }
 
   // TODO: what is third parameter used for?
-  Meteor.call('flightsByQuery', query, 3000, (err,res)=> {
+  Meteor.call('flightsByQuery', query, 3000, (err, res) => {
     if (err) {
       console.log("Requests was unsuccessfull.")
       console.log(err)
