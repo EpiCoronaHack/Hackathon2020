b647dd3e7b8cd3bbbeaf8fb58ccd00a71a999340
Ivan <ivan.pmartell@hotmail.com>
2020-02-18 18:51:49 -0800

    Added mers and sars fasta files ordered by date, with script

--- Genomics/extracting_specific_viruses_from_fasta.py
+++ Genomics/extracting_specific_viruses_from_fasta.py
@@ -1,25 +1,54 @@
 from Bio import SeqIO
 from random import sample
+import collections
+import time
 import re
 
-def do_add(s, x):
-  l = len(s)
-  s.add(x)
-  return len(s) != l
+class Counter(object):
+    value = 0
 
-N_SAMPLE = 97
-fasta_file = "coronaviridae.total.genbank2fasta.uniq.0125.fasta"
+def do_add(s, x):
+    l = len(s)
+    s.add(x)
+    return len(s) != l
+
+def to_integer(t):
+    Counter.value += 1
+    return 10000000*t.tm_year + 10000*t.tm_yday + Counter.value
+
+N_SAMPLE = 120
+fasta_file = "/Users/ivan/Downloads/coronaviridae.total.genbank2fasta.uniq.0125.fasta"
 
 records_data = set()
-records_full = []
+records_full = {}
 
 for record in SeqIO.parse(fasta_file, 'fasta'):
-    mers = re.search("Middle.*Homo_sapiens", record.id)
-    if mers is not None:
-        if do_add(records_data, record.seq._data):
-            records_full.append(record)
+    #virus = re.search("^Middle.*([12][90][0-9]{2}-[0-9]{2}-[0-9]{2})", record.id)
+    virus = re.search("^SARS.*([12][90][0-9]{2}-[0-9]{2}-[0-9]{2})", record.id)
+    if virus is not None:
+        if 'like' not in record.seq._data:
+            if do_add(records_data, record.seq._data):
+                dt = time.strptime(virus.group(1), "%Y-%m-%d")
+                dt_int = to_integer(dt)
+                records_full[dt_int] = record
+            else:
+                print("Duplicate")
+        else:
+            print("Virus-like but not the same")
 
-sampled_records = sample(records_full, N_SAMPLE)
+sorted_records = collections.OrderedDict(sorted(records_full.items()))
+sorted_records_list = [*sorted_records.values()]
+print(len(sorted_records_list))
 
-with open("reduced_mers.fasta", "w+") as output_handle:
+#random 120
+#sampled_records = sample(sorted_records_list, N_SAMPLE)
+
+#first 120
+#sampled_records = sorted_records_list[:N_SAMPLE]
+
+#last 120
+sampled_records = sorted_records_list[-N_SAMPLE:]
+
+#with open("reduced_mers.fasta", "w+") as output_handle:
+with open("reduced_sars.fasta", "w+") as output_handle:
     SeqIO.write(sampled_records, output_handle, "fasta")
