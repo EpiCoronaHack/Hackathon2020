e1b73832a952f2ace5b108bcdffe78fb320d8c49
Lavneet Puri <lavneetpuri@gmail.com>
2020-02-22 15:33:17 +0000

    i4-linting update

--- Team Flight/Data/flirt.eha.io/client.js
+++ Team Flight/Data/flirt.eha.io/client.js
@@ -34,7 +34,8 @@ socket = new WebSocket('ws://127.0.0.1:3000');
    * Get a sorted list of flight schedule records by departureAirport._id field
    * @param {Object} conf
    * @param {String} conf.deptCode initial departure airport code
-   * @param {Number} conf.size number of flight records to fetch starting with `dept_code`
+   * @param {Number} conf.size number of flight records to fetch
+   * starting with `dept_code`
    */
   async function getPage(deptCode, size) {
     return getRecords({
@@ -42,5 +43,4 @@ socket = new WebSocket('ws://127.0.0.1:3000');
       count: size,
     });
   }
-
-})()
+})();
