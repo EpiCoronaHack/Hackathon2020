a8105dc2230ff5ef11ed350d56ad70527a3958dc
Lavneet Puri <lavneetpuri@gmail.com>
2020-02-22 15:31:05 +0000

    i4-add getRecords function to query backend of specific flight schedules

--- Team Flight/Data/flirt.eha.io/client.js
+++ Team Flight/Data/flirt.eha.io/client.js
@@ -11,7 +11,24 @@
 
 socket = new WebSocket('ws://127.0.0.1:3000');
 
-socket.send(JSON.stringify(page));
+  /**
+   * Query backend for flight schedule
+   * @param {Object} conf
+   * @param {Object} conf.query refer to README to construct this mongo query
+   * @param {Number} [conf.count=0] limit number of records;
+   * fetch all if left empty
+   * @return {Array} list of requested flight schedule records
+   */
+  function getRecords({ query, count }) {
+    return new Promise((resolve, reject) => {
+      // Setting count to 0 fetched all records from the server.
+      const limit = count || 0;
+      Meteor.call('flightsByQuery', query, limit, (err, res) => {
+        if (err) reject(err);
+        resolve(res.flights);
+      });
+    });
+  }
 
 
 })()
